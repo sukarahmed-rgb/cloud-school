613f4bdfe658a8e0a521034eacfca3dd24287263
Cloud School Dev <dev@cloudschool.local>
2026-06-19 19:28:06 +0300

    Debug: test without babel transform, add sanity test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,4 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/tests/unit/**/*.test.js'],
   verbose: true,
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
 };
---
+++ tests/unit/simple.test.js
@@ -0,0 +1,5 @@
+describe('Simple sanity check', () => {
+  test('Jest runs', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
